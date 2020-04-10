94b28533335c97a223ec574345da117a75581a1c
NicoLaval <nicolas.laval@insee.fr>
2020-04-10 15:48:24 +0200

    Fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
         '@storybook/addon-actions',
         '@storybook/addon-links',
         '@storybook/addon-knobs',
+        '@storybook/addon-docs',
     ],
     webpackFinal: async (config, { configType }) => {
         // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
