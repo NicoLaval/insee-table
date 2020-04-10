42d0fdfa15474d0a4d3c5cedcfeb3a1e816dff57
Aude Mulliez <aude@iMac-de-Aude.local>
2020-04-10 16:42:36 +0200

    header

--- src/insee-table/components/commons-props.js
+++ src/insee-table/components/commons-props.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 export const columnsProps = PropTypes.arrayOf(
     PropTypes.shape({
         key: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
         component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
     })
 )
